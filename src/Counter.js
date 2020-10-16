f523117648ccebe134f10f662b71a4125d12e03e
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-16 15:40:45 -0400

    build class component; set state by constructor; pass props by attribute; two way handle state

---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: props.initialCount,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <button onClick={() => this.changeCount(-1)}>-</button>
+        <span>{this.state.count}</span>
+        <button onClick={() => this.changeCount(+1)}>+</button>
+      </div>
+    );
+  }
+
+  changeCount(amount) {
+    // reassign state
+    // this.setState({ count: this.state.count + amount });
+
+    // if want to use previous state, use function version to handle state
+    this.setState((prevState) => {
+      return { count: prevState.count + amount };
+    });
+  }
+}
