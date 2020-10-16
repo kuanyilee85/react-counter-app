f787b068e5a3bcb5d7b30988576302b50cdffaa0
kuanyilee85 <kuanyilee85@gmail.com>
2020-10-16 17:08:13 -0400

    Hooks: useContext

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './Counter';
 import CounterHooks from './CounterHooks';
 
+export const ThemeContext = React.createContext();
+
 function App() {
   console.log('Render app');
+  const [theme, setTheme] = useState('red');
   return (
-    <>
+    <ThemeContext.Provider value={{ backgroundColor: theme }}>
       <h2>Counter</h2>
       <Counter initialCount={0} />
       <h2>CounterHooks</h2>
       <CounterHooks initialCount={5} />
-    </>
+      <button
+        onClick={() =>
+          setTheme((prevTheme) => {
+            return prevTheme === 'red' ? 'blue' : 'red';
+          })
+        }
+      >
+        Toggle Theme
+      </button>
+    </ThemeContext.Provider>
   );
 }
 
